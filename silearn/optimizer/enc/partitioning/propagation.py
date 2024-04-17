1b2c79c4b56953a796fb753648c256215a0a5bf6
zhangjingyun7 <117434477+zhangjingyun7@users.noreply.github.com>
2024-04-17 15:48:54 +0800

    Update propagation.py

--- silearn/optimizer/enc/partitioning/propagation.py
+++ silearn/optimizer/enc/partitioning/propagation.py
@@ -35,7 +35,7 @@ class OperatorPropagation(Operator):
 
     @staticmethod
     def reduction_edge(edges, edge_transform, *weights):
-        cnt_e = edge_transform.max() + 1#边处于社区位置的种类（社区内、割边）数，edge_transform为每条边的位置类型编号
+        cnt_e = edge_transform.max() + 1
         e1 = torch.zeros(size=(cnt_e, edges.shape[1]),
                          dtype=edges.dtype,
                          device=edges.device)
@@ -44,30 +44,30 @@ class OperatorPropagation(Operator):
         # print(e1.shape)
 
         edges = e1.scatter(0,
-                           edge_transform.reshape(-1, 1).repeat((1, 2)), edges)#edges根据索引重新分配到e1中,dim=0,索引是 边位置编号edge_transform变为一列且重复一列(让edges一行一起分配),其实就是edges去重
+                           edge_transform.reshape(-1, 1).repeat((1, 2)), edges)
         ret = [edges
-               ] + [silearn.scatter_sum(w, edge_transform) for w in weights] #根据边处在社区位置的类型把边权加起来，同一个社区中的会double
+               ] + [silearn.scatter_sum(w, edge_transform) for w in weights]
         return tuple(ret)
 
     @staticmethod
     def get_edge_transform(edges, identical_flag=False):
-        max_id = int(edges[:, 1].max() + 1) #此时的edges是社区下标，max_id是社区数
+        max_id = int(edges[:, 1].max() + 1)
         bd = 1
         shift = 0
         while bd <= max_id:
             bd = bd << 1
             shift += 1  #社区数<2^bd
         # todo: hash if shift is too big
-        edge_hash = (edges[:, 0] << shift) + edges[:, 1]  #边右顶点社区的下标+边左顶点社区下标*2^shift  从二进制看就是左部分为左顶点社区，有右部分为右顶点社区
+        edge_hash = (edges[:, 0] << shift) + edges[:, 1]
 
         if identical_flag:
             _, transform, counts = torch.unique(edge_hash,
                                                 return_inverse=True,
-                                                return_counts=True)  #count为_中的每个元素在原始张量中的数量，transform为原始张量在_上索引
+                                                return_counts=True)
             flag = counts[transform] != 1
             return transform, flag
         else:
-            _, transform = torch.unique(edge_hash, return_inverse=True)  #_为独立不重复元素组成张量，transform为原始张量在_上索引，边的左右顶点在的社区一样表示两条边当前在的社区位置相同（社区内、割边）
+            _, transform = torch.unique(edge_hash, return_inverse=True)
             return transform
 
     @staticmethod
@@ -77,7 +77,7 @@ class OperatorPropagation(Operator):
             wm = [i[operation_ptrs] for i in weights]
             trans = OperatorPropagation.get_edge_transform(em)
             redu = OperatorPropagation.reduction_edge(em, trans, *wm)
-            cnt = redu[0].shape[0]#边处于社区位置的种类数（社区内，不同社区间割边）
+            cnt = redu[0].shape[0]
             # operation_ptrs = operation_ptrs[:cnt]
             # skip_ptrs = operation_ptrs[cnt:]
             # edges[operation_ptrs] = 0
@@ -90,7 +90,7 @@ class OperatorPropagation(Operator):
             return ret
         else:
             trans = OperatorPropagation.get_edge_transform(edges)
-            return OperatorPropagation.reduction_edge(edges, trans, *weights)#返回的是一个元组 边处于社区位置的种类 以及每个种类下的边权和
+            return OperatorPropagation.reduction_edge(edges, trans, *weights)
 
     @staticmethod
     def sum_up_multi_edge_ts(edges, *weights, operation_ptrs=None):
@@ -149,7 +149,7 @@ class OperatorPropagation(Operator):
         merge_all = False
         if contains_self_loops is None:
             # noinspection PyUnresolvedReferences
-            contains_self_loops = bool((edges[:, 0] == edges[:, 1]).any())  #连接自己的边
+            contains_self_loops = bool((edges[:, 0] == edges[:, 1]).any())
 
         # v1 = None
         vst = None
@@ -183,15 +183,15 @@ class OperatorPropagation(Operator):
             transs.append(trans)
 
         while operated_cnt > ter or not merge_all:
-            max_operate_cnt = math.ceil((current_num_vertices - min_com) * p)#（当前顶点数-最小社区数）*1 最多需要进行的操作数
+            max_operate_cnt = math.ceil((current_num_vertices - min_com) * p)
             #print('current_num_vertices:{},max_operate_cnt:{}'.format(current_num_vertices,max_operate_cnt))
-            edge_s = edges[:, 0]#左一列顶点
-            edge_t = edges[:, 1]#右一列顶点
+            edge_s = edges[:, 0]
+            edge_t = edges[:, 1]
 
-            mx = torch.max(edges)#最大顶点下标，+1就是顶点/社区数了
+            mx = torch.max(edges)
             #print('mx:{},operated_cnt:{}'.format(mx,operated_cnt))
             if operated_cnt <= ter + 1:
-                merge_all = True  #上一轮的merge操作数小于等于1
+                merge_all = True 
             # print(trans_prob.shape)
             # print(self.graph.edge_index[1].shape)
             # print("time-x:{}".format(time.time() - t))
@@ -201,10 +201,10 @@ class OperatorPropagation(Operator):
             if not cache:
                 v1 = silearn.scatter_sum(trans_prob,
                                          edge_t,
-                                         clip_length=mx + 1).reshape(-1) #按右一列顶点求di
+                                         clip_length=mx + 1).reshape(-1) 
 
                 # print(self.enc.graph.stationary_dist)
-                vst = v1[edges] #每条边的左右顶点/社区的di/vol(a)
+                vst = v1[edges] 
 
 
 
@@ -212,10 +212,10 @@ class OperatorPropagation(Operator):
                 if not cache:
                     g1 = silearn.scatter_sum(trans_prob * (edge_s != edge_t),
                                              edge_t,
-                                             clip_length=mx + 1) #社区割边
+                                             clip_length=mx + 1) 
 
-                    gst = g1[edges]# 割边数变成edges形式
-                    vx = vst.sum(dim=1) #每一条边连接的两个社区的vol(α)求和 ，每一行求和
+                    gst = g1[edges]
+                    vx = vst.sum(dim=1) 
 
                     # gx = gst.sum(dim = 1)
 
@@ -229,15 +229,14 @@ class OperatorPropagation(Operator):
                     #         g0s = g0[edges[:, 0]]
                     #         g0t = g0[edges[:, 1]]
                     #         g0x = g0s + g0t
-                    vin = vst - gst #每一条边连接的两个社区的vol(α)-gα 即社区内部边权之和
+                    vin = vst - gst 
 
                     dH1 = (vin[:, 0]) * torch.log2(
                         vst[:, 0]) + (vin[:, 1]) * torch.log2(vst[:, 1]) - (
-                            vin[:, 0] + vin[:, 1]) * torch.log2(vx)  # 左社区的 (vol(α)-gα )*log2 vol(α) +右社区的(vol(β)-gβ )*log2 vol(β)-(vol(α)-gα+vol(β)-gβ )*log2 vol(α+β)
-                    dH2 = 2 * trans_prob * ((self._log2m) - torch.log2(vx)) #2*(log2 vol(G)-log2 vol(α+β) )*边权
+                            vin[:, 0] + vin[:, 1]) * torch.log2(vx)  
+                    dH2 = 2 * trans_prob * ((self._log2m) - torch.log2(vx))
                     dH0 = dH1 + dH2 #Δ
                     op = (dH0 > 0)
-                    # 使用M选择合并时
                     # if srt_M:
                     #     dH = - dH2 / dH1 # * (self._log2m)
                     # print(dH1.min())
@@ -258,8 +257,8 @@ class OperatorPropagation(Operator):
                     op = (dH >= torch.median(dH[op]))
             else:
                 dH = trans_prob * (self._log2m -
-                                   torch.log2(vst[:, 0] + vst[:, 1])) #ew*(log2 vol(G)- log2(di+dj) )
-                op = (dH > torch.median(dH[dH > 0])) #选大于0且大于等于中位数的那几条边
+                                   torch.log2(vst[:, 0] + vst[:, 1])) 
+                op = (dH > torch.median(dH[dH > 0])) 
 
             cache = True
 
@@ -278,32 +277,32 @@ class OperatorPropagation(Operator):
             hash_t = edge_t * 10007 // 1009
 
             merge = torch.logical_and(
-                merge,   #dH大于中位数大于0 且满足下面条件（似乎是去掉重复边）
-                torch.logical_or((vst[:, 0] < vst[:, 1]),  #左顶点di<右顶点di  or  左顶点di=右顶点di 且 左顶点hash<右顶点hash
+                merge,   
+                torch.logical_or((vst[:, 0] < vst[:, 1]), 
                                  torch.logical_and((vst[:, 0] == vst[:, 1]),
                                                    hash_x < hash_t)))
             if not torch.any(merge):
-                operated_cnt = 0 #没有一个要merge
+                operated_cnt = 0
                 continue
 
             # t = time.time()
             id0 = edge_s
             id1 = edge_t
-            if not merge_all:   #还没merge完
+            if not merge_all:
                 dH = dH * (1.0 + 1e-6 * hash_x * 1e-6 * hash_t) #dH=dH*(1+hash_x/10^6 * hash_t/10^6)
 
-            id0 = id0[merge]  #要merge的边的左顶点
-            id1 = id1[merge]  #要merge的边的右顶点
+            id0 = id0[merge]
+            id1 = id1[merge] 
 
 
-            dH = dH[merge] #要merge的边的dH
-            _, dH_amax = silearn.scatter_max(dH, id0)  #顶点有多条边要merge选个最好的，dH_amax放最终要merge的边在dH中索引，如id0[dH_amax[i]]和id1[dH_amax[i]]merge
+            dH = dH[merge]
+            _, dH_amax = silearn.scatter_max(dH, id0)  
             # dh_amax[i] = (argmax_j[dH[j]] for id0[j] = i) if \exist i in id0, else dH.shape + 1
             # dH_amax[i] is unique
 
             dH_amax = dH_amax[dH_amax <
-                              dH.shape[0]]  # then dH_amax is a unique set
-            operated_cnt = int(dH_amax.shape[0])  # cuda synchronized  要进行merge操作的次数
+                              dH.shape[0]]  
+            operated_cnt = int(dH_amax.shape[0])  
 
             if operated_cnt == 0:
                 continue
@@ -312,7 +311,7 @@ class OperatorPropagation(Operator):
             # print(max_operate_cnt)
             # print(operated_cnt)
 
-            # 双向max
+
             if operated_cnt > max_operate_cnt and di_max:
                 id0 = id0[dH_amax]
                 id1 = id1[dH_amax]
@@ -331,15 +330,15 @@ class OperatorPropagation(Operator):
             # operated_cnt = ddd_new
 
             ids = id0[dH_amax]
-            idt = id1[dH_amax]  #要merge的左右顶点对
-            trans = torch.arange(edges.max() + 1, device=self.enc.graph.device)#合并前的社区划分(一人一个）
+            idt = id1[dH_amax] 
+            trans = torch.arange(edges.max() + 1, device=self.enc.graph.device)
             trans[ids] = trans[idt]
             # if operated_cnt < 10:
             #     altered = torch.zeros(self.enc.graph.num_vertices, device=self.enc.graph.device, dtype=torch.bool)
             #     altered[ids] = True
             #     altered[idt] = True
             #     altered_e = torch.nonzero(torch.logical_or(altered[edges[:, 0]],
-            #                                  altered[edges[:, 1]]))  # edges
+            #                                  altered[edges[:, 1]]))  
             #
             # else:
             #     altered_e = None
@@ -353,7 +352,7 @@ class OperatorPropagation(Operator):
             for i in range(int(lg_merge)):
                 # ids[var] = trans[trans[var]]
                 # old = trans[ids]
-                trans[ids] = trans[trans[ids]]  #合并完的社区划分
+                trans[ids] = trans[trans[ids]]  
                 # ids = torch.nonzero(trans[ids] != old)
 
             # print(time.time() - t)
@@ -371,7 +370,7 @@ class OperatorPropagation(Operator):
             # print(torch.nonzero(trans != torch.arange(self.enc.graph.num_vertices)))
 
             # compress id allocation
-            trans = torch.unique(trans, return_inverse=True)[1] #trans中每个在独立不重复张量中索引，即在第几个社区
+            trans = torch.unique(trans, return_inverse=True)[1] 
             # _, trans, counts= torch.unique(trans, return_inverse=True, return_counts = True)
 
             # self.community_result = trans
@@ -385,7 +384,7 @@ class OperatorPropagation(Operator):
             if adj_cover is None:
                 cache = False
 
-                edges = trans[edges] #edges从顶点下标变为社区下标
+                edges = trans[edges] 
                 edges, trans_prob = OperatorPropagation.sum_up_multi_edge(
                     edges, trans_prob)
 
