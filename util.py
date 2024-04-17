f911df6d6cf58fce13e597f0c9beda936b4c6b49
zhangjingyun7 <117434477+zhangjingyun7@users.noreply.github.com>
2024-04-17 15:42:59 +0800

    Update util.py

--- util.py
+++ util.py
@@ -4,7 +4,6 @@ import csv
 import os
 import torch
 
-# 导入用户
 def load_user(filename, b):
     print("is loading user from {}...".format(filename))
     name_file = open(filename, 'r', encoding='utf-8')
@@ -25,13 +24,12 @@ def load_user(filename, b):
     print('node:', num)
     return account, real_bot, ff
 
-#从.tsv导入标签
 def load_label(filename):
     fp = open(filename + '/' + filename + '.tsv', 'r', encoding='utf8')
-    csv_file = open(filename + "/label.csv", "w+", newline='')  # 新建csv格式文件
-    writer = csv.writer(csv_file)  # 对象化
-    header = ["userid", "label"]  # 构造表头
-    writer.writerow(header)  # 写入表头
+    csv_file = open(filename + "/label.csv", "w+", newline='')
+    writer = csv.writer(csv_file)
+    header = ["userid", "label"]
+    writer.writerow(header)
     for line in fp:
         line = line.strip('\n').split('\t')
         id = line[0]
@@ -41,7 +39,7 @@ def load_label(filename):
             label = 1
         writer.writerow([id, label])
 
-# 导入tweet并处理(cresci15以及cresci17中fake follower)
+
 def load_tweet(filename, account):
     print("is loading tweet from {}...".format(filename))
     tweet_file = open(filename, 'r', encoding='utf-8')
@@ -49,13 +47,13 @@ def load_tweet(filename, account):
     n = 0
     num = len(account)
     types = np.zeros((num, 3))  # original,retweet,comment
-    inf = np.zeros(num)  # 原创推文的评论点赞转发数总
+    inf = np.zeros(num)
     for data in datas:
         n = n + 1
         if n == 1:
             continue
         else:
-            t = 0  # 找user_id在的列
+            t = 0 
             while t < 6 and t < len(data):
                 if data[t].isdigit() and (data[t] in account):
                     break
@@ -65,7 +63,6 @@ def load_tweet(filename, account):
                 continue
             x = account.index(data[t])
 
-            # 统计三种类型推文
             if data[2].startswith('RT @'):
                 types[x][1] += 1
             elif data[2].startswith('@'):
@@ -77,22 +74,22 @@ def load_tweet(filename, account):
                 if data[-6].isdigit():
                     inf[x] += int(data[-6])
                 if data[-5].isdigit():
-                    inf[x] += int(data[-5])  # 原创推文的评论点赞转发数总和
+                    inf[x] += int(data[-5])
     print('')
     for i in range(num):
         if types[i][0] != 0:
             inf[i] = inf[i] / types[i][0]
-        sum_t = sum(types[i])  # 总推文数
+        sum_t = sum(types[i])
         if sum_t != 0:
             types[i] = types[i] / sum_t
     return types, inf
 
 
 def process_json(filename):
-    csv_file = open(filename + "/" + filename + ".csv", "w+", newline='')  # 新建csv格式文件
-    writer = csv.writer(csv_file)  # 对象化
-    header = ["userid", "screenname", "follower", "friend"]  # 构造表头
-    writer.writerow(header)  # 写入表头
+    csv_file = open(filename + "/" + filename + ".csv", "w+", newline='')
+    writer = csv.writer(csv_file)
+    header = ["userid", "screenname", "follower", "friend"]
+    writer.writerow(header)
     fp = open(filename + '/' + filename + '_tweets.json', 'r', encoding='utf8')
     for record in ijson.items(fp, 'item'):
         id = '\'' + str(record["user_id"])
@@ -118,7 +115,6 @@ def process_tweet(filename):
     if not os.path.exists(path_label):
         load_label(filename)
     id = []
-    # 关注粉丝比
     ff = []
     fp_ff = open(path_followers, 'r', encoding='utf-8')
     ffs = csv.reader(fp_ff)
@@ -129,7 +125,6 @@ def process_tweet(filename):
             continue
         id.append(data[0].replace('\'', ''))
         ff.append((int(data[3]) + 1) / (int(data[2]) + 1))
-    # 真实标签
     num = len(id)
     label = np.zeros(num)
     fp_label = open(path_label, 'r', encoding='utf8')
@@ -144,9 +139,8 @@ def process_tweet(filename):
             label[x] = data[1]
 
     print(len(id), len(label), len(ff))
-    # 发帖类型分布，发帖影响力
     types = np.zeros((num, 3))  # original,retweet,comment
-    infs = np.zeros(num)  # 推文的评论点赞转发数总
+    infs = np.zeros(num)
     fp_tweet = open(path_tweets, 'r', encoding='utf8')
     '''
     fp_tweet1 = fp_tweet.read()
@@ -172,18 +166,17 @@ def process_tweet(filename):
                 text = data[i].split('?')
                 infs[x] += int(text[-1])
                 flag += 1
-    # 写入文件中
-    with open(filename + '/' + filename + '_f.csv', "w+", newline='') as out_file:  # 新建csv格式文件
-        writer = csv.writer(out_file)  # 对象化
-        header = ["userid", "label", "infs", "ff", "type_o", "type_r", "type_c"]  # 构造表头
-        writer.writerow(header)  # 写入表头
+    with open(filename + '/' + filename + '_f.csv', "w+", newline='') as out_file:
+        writer = csv.writer(out_file)
+        header = ["userid", "label", "infs", "ff", "type_o", "type_r", "type_c"]
+        writer.writerow(header)
         for i in range(num):
             if sum(types[i]) != 0:
                 infs[i] = infs[i] / sum(types[i])
                 types[i] = types[i] / sum(types[i])
             datarow = [id[i], label[i], infs[i], ff[i]]
             datarow.extend(types[i])
-            writer.writerow(datarow)  # 写入csv
+            writer.writerow(datarow)
 
 
 def multirank_cuda(multi_graph):
@@ -259,47 +252,3 @@ def multirank(multi_graph):
             break
     print('y_final:', y_new)
     return x_new, y_new
-
-def multirank_old(multi_graph):
-    # print('----------------------------------------------')
-    print('is calculating multirank...')
-    m = len(multi_graph[0])
-    r = len(multi_graph)
-    O = np.zeros((m, m, r))
-    R = np.zeros((r, m, m))
-    sum_j = np.zeros((m, m))
-    sum_i1 = np.zeros((r, m))
-    for i1 in range(m):
-        for i2 in range(m):
-            for j in range(r):
-                sum_i1[j][i2] += multi_graph[j][i1][i2]
-                sum_j[i1][i2] += multi_graph[j][i1][i2]
-    for i1 in range(m):
-        for i2 in range(m):
-            for j in range(r):
-                O[i1][i2][j] = multi_graph[j][i1][i2] / sum_i1[j][i2] if sum_i1[j][i2] != 0 else 1 / m
-                R[j][i1][i2] = multi_graph[j][i1][i2] / sum_j[i1][i2] if sum_j[i1][i2] != 0 else 1 / r
-    x_old = np.ones(m)
-    x_new = np.ones(m)
-    x_old = x_old * (1 / m)
-    y_old = np.ones(r)
-    y_new = np.ones(r)
-    y_old = y_old * (1 / r)
-    x_new[:] = x_old[:]
-    y_new[:] = y_old[:]
-    while True:
-        x_old[:] = x_new[:]
-        y_old[:] = y_new[:]
-        for i in range(m):
-            x_new[i] = np.dot(np.dot(x_old, O[i]), y_old.T)
-        for j in range(r):
-            y_new[j] = np.dot(np.dot(x_old, R[j]), x_old.T)
-        z = np.sqrt(sum((x_old - x_new) ** 2)) + np.sqrt(sum((y_old - y_new) ** 2))
-        if z < 0.004:
-            break
-    print('y_final:', y_new)
-    return x_new, y_new
-
-
-
-
