fff95962a70973f749dbb7bde51b18e0b321a5f5
zhangjingyun7 <117434477+zhangjingyun7@users.noreply.github.com>
2024-04-17 15:41:03 +0800

    Update main.py

--- main.py
+++ main.py
@@ -25,7 +25,7 @@ def process(filename):
     label = []
     ff = []
     types = []  # original,retweet,comment
-    infs = []  # 原创推文的评论点赞转发数总
+    infs = []
     line = 0
     for data in datas:
         line += 1
@@ -47,7 +47,7 @@ def process_human(dataset, filename):
         label = []
         ff = []
         types = []  # original,retweet,comment
-        infs = []  # 原创推文的评论点赞转发数总
+        infs = []
         line = 0
         file0 = open(path, 'r', encoding='utf-8')
         datas = csv.reader(file0)
@@ -68,7 +68,6 @@ def process_human(dataset, filename):
 def test(filename,fig_network=False,fig_tree=False):
     num0, id0, label0, ff0, types0, infs0 = process(filename)
     num1, id1, label1, ff1, types1, infs1 = process_human('cresci-2015', 'E13')  #botwiki:TFP[:65],pronbots::E13
-    # 合并人类账户和机器账户
     num = num0 + num1
     id_block = np.array(id0+id1)
     label_block = np.array(label0+label1)
@@ -78,7 +77,6 @@ def test(filename,fig_network=False,fig_tree=False):
     print('num:{},bot:{}'.format(num,np.sum(label_block)))
     
 
-    # 构图
     graph = np.zeros((3, num, num))
     edge = np.zeros(3)
 
@@ -132,12 +130,10 @@ def test(filename,fig_network=False,fig_tree=False):
         adj_matrix[i,i]=0.01   
     
     
-    #计算mulrirank
     #x, y = multirank_cuda(graph)
     x, y = multirank(graph)
     #x, y = multirank_cuda(graph[[1,2]])
 
-    #编码树
     edges = np.array(adj_matrix.nonzero())  # [2, E]
     ew = adj_matrix[edges[0, :], edges[1, :]]
     devices = 'cuda:0' if torch.cuda.is_available() else 'cpu'
@@ -167,7 +163,6 @@ def test(filename,fig_network=False,fig_tree=False):
     value = np.zeros(num)
     num_bot_comm =0
     bot_list=[]
-    #社区划分
     for i in comms.keys():
         comm = comms[i]
         n_bots = 0
@@ -191,7 +186,6 @@ def test(filename,fig_network=False,fig_tree=False):
                 value[node] = n_beta
         bot_rate.append([n_bots / n_nodes, n_bots, n_nodes, n_x / n_nodes, comm_SE, n_beta])
     
-    # 准确率计算
     acc = metrics.accuracy_score(label_block, pre_bot)
     precision = metrics.precision_score(label_block, pre_bot)
     recall = metrics.recall_score(label_block, pre_bot)
@@ -204,178 +198,6 @@ def test(filename,fig_network=False,fig_tree=False):
     print('F1:{}'.format(f1))
     print('AUC:{}'.format(auc_score))
 
-    fsize=25
-    plt.figure(figsize=(8,6))
-    plt.plot(fpr, tpr, label=f'AUC = {auc_score:.2f}', linewidth=5)
-    # 随即分类器没有分类能力，其FPR=TPR。随机分类器的性能通常表示为ROC曲线上的对角线
-    plt.plot([0, 1], [0, 1], linestyle='--', color='r', label='Random')
-    plt.xlabel('False Positive Rate', fontsize=fsize)
-    plt.ylabel('True Positive Rate', fontsize=fsize)
-    plt.title('ROC Curve', fontsize=fsize)
-    plt.xticks(fontsize=fsize)
-    plt.yticks(fontsize=fsize)
-    plt.grid(True)
-    plt.legend(loc='lower right',fontsize=fsize)
-    plt.savefig('./figure/ROC_{}_1.png'.format(filename), bbox_inches='tight')                
-    plt.savefig('./figure/ROC_{}_1.pdf'.format(filename), bbox_inches='tight')
-
-
-    '''
-    for b_data in bot_rate:
-        if b_data[5] >= 0.6:
-            print(b_data)
-    print('')
-    for b_data in bot_rate:
-        if b_data[5] < 0.6:
-            print(b_data)
-    '''
-    #画网络图
-    if fig_network==True:
-        k=0.5
-        print('calculating pos...')
-        pos = nx.spring_layout(G, k=k, iterations=30,seed=1721)
-        fig, ax = plt.subplots(figsize=(10,10))
-        #ax.axis("off")
-        plt.xlim(-1.05,1.05)
-        plt.ylim(-1.05,1.05)
-        color=[]
-        for item in pre_bot:
-        #for item in label_block:
-            if item==1:
-                color.append('darkred')
-            else:
-                color.append('royalblue')
-        print('drawing figure...')
-        nx.draw_networkx_nodes(G, pos=pos, ax=ax, alpha=1, node_color=color, node_size=15)
-        #for bot in bot_list:
-        #    plt.scatter(pos[bot.item()][0],pos[bot.item()][1],s=15,c='darkred',alpha=0.8,marker='o')
-        plt.scatter([], [], marker='o', c='darkred', s=30, alpha=1, label='bot')
-        plt.scatter([], [], marker='o', c='royalblue', s=30, alpha=1, label='human')
-        plt.legend(scatterpoints=1, fontsize=18, loc='upper right')
-        plt.savefig('./figure/network_pred_{}_withhuman_k{}.pdf'.format(filename,k), bbox_inches='tight')
-        plt.savefig('./figure/network_pred_{}_withhuman_k{}.png'.format(filename,k), bbox_inches='tight')
-
-    # 画树
-    if fig_tree == True:
-        size = [10, 15, 20, 35, 50, 60, 85, 110, 135, 160]  # <=1,9,19,49,99,199,499,999,1999,>=2000
-        fig = plt.figure(figsize=(10, 4))
-        plt.xticks([])  # 去掉x轴
-        plt.yticks([])  # 去掉y轴
-
-        # root
-        x0 = 0.5
-        y0 = 1
-        y1 = 0.8
-        s = size[7]
-        plt.scatter(x0, y0, marker='s', c='gray', edgecolor='k', linewidth='0.5', s=s+10, zorder=2)
-        plt.text(x0-0.05, y0, 'Root', ha='center', va='center', fontsize=9, color='k')
-        #
-        num_f_bot = []
-        num_f_human=[]
-        num_t_bot = []
-        num_t_human=[]
-        for i in range(totol_comm):
-            num_t_bot.append(bot_rate[i][1])
-            num_t_human.append(bot_rate[i][2]-bot_rate[i][1])
-            num_f_bot.append(math.ceil(bot_rate[i][1] / 400))
-            num_f_human.append(math.ceil((bot_rate[i][2]-bot_rate[i][1]) / 400))
-        print(num_f_bot)
-        print(num_f_human)
-        index = np.argsort(-(np.array(num_t_bot)+np.array(num_t_human)))
-        print(index)
-        sum_f = sum(num_f_bot)+sum(num_f_human)
-        dx = 1 / (num_bot_comm + sum_f + 1)
-        x2 = 0
-        y2 = 0.6
-        plt.text(0.05, y1, 'Comm', ha='center', va='center', fontsize=9, color='k')
-        for i in index:
-            x2 += dx
-            x1 = x2 + (num_f_bot[i]+num_f_human[i] - 1) * dx / 2
-            if num_t_bot[i]+num_t_human[i] >= 2000:
-                s = size[9]
-            elif num_t_bot[i]+num_t_human[i] > 999:
-                s = size[8]
-            elif num_t_bot[i]+num_t_human[i] > 499:
-                s = size[7]
-            elif num_t_bot[i]+num_t_human[i] > 199:
-                s = size[6]
-            elif num_t_bot[i]+num_t_human[i] > 99:
-                s = size[5]
-            elif num_t_bot[i]+num_t_human[i] > 49:
-                s = size[4]
-            elif num_t_bot[i]+num_t_human[i] > 19:
-                s = size[3]
-            elif num_t_bot[i]+num_t_human[i] > 9:
-                s = size[2]
-            #elif num_t_bot[i]+num_t_human[i] > 1:
-                #s = size[1]
-            else:
-                s = size[0]
-            plt.plot([x0, x1], [y0, y1], color='lightgrey', linewidth=0.5, zorder=1)
-            if bot_rate[i][5] >= 0.6:
-                color = 'darkred'
-                plt.scatter(x1, y1, marker='s', c=color, edgecolor='k', linewidth='0.5', s=s + 10, zorder=2)
-            else:
-                color = 'royalblue'
-                plt.scatter(x1, y1, marker='s', c=color, edgecolor='k', linewidth='0.5', s=s + 10, zorder=2)
-            plt.text(x1, y1 + 0.03, '{}'.format(num_t_bot[i]+num_t_human[i]), ha='center', va='center', fontsize=6, color='k')
-            if num_f_bot[i]+num_f_human[i] != 1:
-                for j in range(num_f_bot[i]):
-                    if j != (num_f_bot[i] - 1) or (num_t_bot[i] % 400 == 0):
-                        s = size[5]
-                    elif num_t_bot[i] % 400 > 199:
-                        s = size[4]
-                    elif num_t_bot[i] % 400 > 99:
-                        s = size[3]
-                    elif num_t_bot[i] % 400 > 49:
-                        s = size[2]
-                    elif num_t_bot[i] % 400 > 9:
-                        s = size[1]
-                    else:
-                        s = size[0]
-                    plt.plot([x1, x2], [y1, y2], color='lightgrey', linewidth=0.5, zorder=1)
-                    plt.scatter(x2, y2, marker='o', c='indianred', edgecolor='k',linewidth='0.5', s=s, zorder=2)
-                    x2 += dx
-                for j in range(num_f_human[i]):
-                    if j != (num_f_human[i] - 1) or num_t_human[i] % 300 == 0:
-                        s = size[5]
-                    elif num_t_bot[i] % 400 > 199:
-                        s = size[4]
-                    elif num_t_bot[i] % 400 > 99:
-                        s = size[3]
-                    elif num_t_bot[i] % 400 > 49:
-                        s = size[2]
-                    elif num_t_bot[i] % 400 > 9:
-                        s = size[1]
-                    else:
-                        s = size[0]
-                    plt.plot([x1, x2], [y1, y2], color='lightgrey', linewidth=0.5, zorder=1)
-                    plt.scatter(x2, y2, marker='o', c='cornflowerblue', edgecolor='k',linewidth='0.5', s=s, zorder=2)
-                    x2 += dx
-            else:
-                plt.plot([x1, x2], [y1, y2], color='lightgrey', linewidth=0.5, zorder=1)
-                if num_t_bot[i]+num_t_human[i] % 400 == 0:
-                    s = size[5]
-                elif num_t_bot[i] % 400 > 199:
-                    s = size[4]
-                elif num_t_bot[i] % 400 > 99:
-                    s = size[3]
-                elif num_t_bot[i] % 400 > 49:
-                    s = size[2]
-                elif num_t_bot[i] % 400 > 9:
-                    s = size[1]
-                else:
-                    s = size[0]
-                if num_f_bot[i]==1:
-                    plt.scatter(x2, y2, marker='o', c='indianred', edgecolor='k',linewidth='0.5', s=s, zorder=2)
-                else:
-                    plt.scatter(x2, y2, marker='o', c='cornflowerblue', edgecolor='k',linewidth='0.5', s=s, zorder=2)
-        plt.scatter([], [], marker='s', c='darkred', edgecolor='k', linewidth='0.5', s=35, label='bot community')
-        plt.scatter([], [], marker='s', c='royalblue', edgecolor='k', linewidth='0.5', s=35, label='human community')
-        plt.scatter([], [], marker='o', c='indianred', edgecolor='k', linewidth='0.5', s=35, label='bots')
-        plt.scatter([], [], marker='o', c='cornflowerblue', edgecolor='k', linewidth='0.5', s=35, label='humans')
-        plt.legend(scatterpoints=1, fontsize=10, loc='upper left')
-        plt.savefig('./figure/tree_{}_withhuman.pdf'.format(filename), bbox_inches='tight')
 
 
 if __name__ == "__main__":
